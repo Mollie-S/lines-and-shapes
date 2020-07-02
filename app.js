b0c9932e4697273e36f497f37e02c21ea012ee40
Mollie-S <olcha.olcha@gmail.com>
2020-07-02 17:53:10 +0200

    Fixed the position of the circles on touch screens.

--- app.js
+++ app.js
@@ -1,61 +1,70 @@
-const lastPosition = {};
+// window.addEventListener("DOMContentLoaded", init);
 
-let isDrawing = false;
-
+// function init() {
 let canvas = document.querySelector("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
 const context = canvas.getContext("2d");
 
-function draw(event) {
-  if (!isDrawing) return;
-
-  context.beginPath();
-
-  let randomColor = Math.floor(Math.random() * 16277216).toString(16);
-  context.arc(lastPosition.x, lastPosition.y, 10, 0, Math.PI * 2, true);
-  context.fill();
-  context.fillStyle = `#${randomColor}`;
-
-  lastPosition.x = Math.random() * 10 + event.clientX;
-  lastPosition.y = Math.random() * 10 + event.clientY;
-}
-
-function getTouchPosition(event) {
-  if (event.touches) {
-    if ((event.touches.length = 1)) {
-      const touch = event.touches[0];
-      touchX = touch.pageX - touch.target.offsetLeft;
-      touchY = touch.pageY - touch.target.offsetTop;
-    }
-  }
-}
-
-function drawOnTouch(event) {
-  getTouchPosition(event);
-
-  context.beginPath();
-
-  let randomColor = Math.floor(Math.random() * 16277216).toString(16);
-  context.arc(lastPosition.x, lastPosition.y, 10, 0, Math.PI * 2, true);
-  context.fill();
-  context.fillStyle = `#${randomColor}`;
-
-  lastPosition.x = Math.random() * 10 + touchX;
-  lastPosition.y = Math.random() * 10 + touchY;
-
-  console.log("hello");
-  event.preventDefault();
-}
+const domRect = canvas.getBoundingClientRect();
+canvas.width = domRect.width;
+canvas.height = domRect.height;
 
 window.addEventListener("mousemove", draw);
 window.addEventListener("mouseup", () => (isDrawing = false));
 window.addEventListener("mousedown", () => {
   isDrawing = true;
-  lastPosition.x = event.clientX;
-  lastPosition.y = event.clientY;
+  lastPosition.x = event.offsetX;
+  lastPosition.y = event.offsetY;
 });
 
-canvas.addEventListener("touchstart", drawOnTouch, false);
-canvas.addEventListener("touchmove", drawOnTouch, false);
+canvas.addEventListener(
+  "touchstart",
+  (event) => drawOnTouch(event, domRect),
+  false
+);
+canvas.addEventListener(
+  "touchmove",
+  (event) => drawOnTouch(event, domRect),
+  false
+);
+// }
+
+const lastPosition = {};
+
+let isDrawing = false;
+
+function createCircles() {
+  context.beginPath();
+
+  let randomColor = Math.floor(Math.random() * 16277216).toString(16);
+  context.arc(lastPosition.x, lastPosition.y, 10, 0, Math.PI * 2, true);
+  context.fill();
+  context.fillStyle = `#${randomColor}`;
+}
+
+function draw(event) {
+  if (!isDrawing) return;
+  createCircles();
+  lastPosition.x = Math.random() * 10 + event.offsetX;
+  lastPosition.y = Math.random() * 10 + event.offsetY;
+
+  console.log(event);
+}
+
+function getTouchPosition(event, domRect) {
+  if (event.touches) {
+    if (event.touches.length === 1) {
+      const touch = event.touches[0];
+      touchX = touch.clientX - domRect.x;
+      touchY = touch.clientY - domRect.y;
+    }
+  }
+}
+
+function drawOnTouch(event, domRect) {
+  getTouchPosition(event, domRect);
+  createCircles();
+  lastPosition.x = Math.random() * 10 + touchX;
+  lastPosition.y = Math.random() * 10 + touchY;
+
+  event.preventDefault();
+}
